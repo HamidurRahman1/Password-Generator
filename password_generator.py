83d91bdca02cd3c72c3bc2fe899e5a5cb3f202a7
Hamidur Rahman <Hamidur0405@gmail.com>
2021-02-25 22:13:59 -0500

    duplicate

--- password_generator.py
+++ password_generator.py
@@ -9,6 +9,8 @@ def generate_password(length=5):
     return "".join(choice(chars) for x in range(randint(5, length)))
 
 
+s = "abcdeadgc"
+print(set(i for i in s if s.count(i) > 1))
+
 p = generate_password(5)
-print(len(p), p)
 
