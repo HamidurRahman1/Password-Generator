ac59cc3c4f39d2d8a4d40e0a3ac28d3a3070e205
Hamidur Rahman <Hamidur0405@gmail.com>
2021-02-27 19:05:05 -0500

    using class

--- password_generator.py
+++ password_generator.py
@@ -2,15 +2,23 @@
 import string
 from random import *
 
-chars = string.punctuation + string.ascii_letters + string.digits
-
-
-def generate_password(length=5):
-    return "".join(choice(chars) for x in range(randint(5, length)))
-
-
 s = "abcdeadgc"
 print(set(i for i in s if s.count(i) > 1))
 
-p = generate_password(5)
+
+class PasswordGenerator:
+
+    __chars = string.punctuation + string.ascii_letters + string.digits
+
+    @staticmethod
+    def __generate_password(length=5):
+        return "".join(choice(PasswordGenerator.__chars) for x in range(randint(5, length)))
+
+    @staticmethod
+    def generate_password(length=5):
+        return PasswordGenerator.__generate_password(length)
+
+
+p = PasswordGenerator.generate_password(5)
+print(p)
 
