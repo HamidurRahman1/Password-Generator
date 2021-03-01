c9d86e1c5dd13529c364f10c7fd8e7d72a5a028a
Hamidur Rahman <Hamidur0405@gmail.com>
2021-02-28 21:33:29 -0500

    update min len to 7

--- password_generator.py
+++ password_generator.py
@@ -11,24 +11,26 @@ class PasswordGenerator:
     __chars = string.punctuation + string.ascii_letters + string.digits
 
     @staticmethod
-    def __generate_password(length=5):
-        """generates a random password of the given length. Length must be 5 or greater"""
+    def __generate_password(length=7):
+        """generates a random password of the given length. Length must be 7 or greater"""
 
         try:
             length = int(length)
-            if length >= 5:
-                return "".join(choice(PasswordGenerator.__chars) for x in range(randint(5, length)))
+            if length >= 7:
+                return "".join(choice(PasswordGenerator.__chars) for x in range(randint(length, length)))
+            else:
+                return "".join(choice(PasswordGenerator.__chars) for x in range(randint(7, 7)))
         except ValueError:
             raise ValueError("Expected integer but given {} for `length`.".format(type(length)))
 
     @staticmethod
-    def generate_password(length=5, from_chars=None):
+    def generate_password(length=7, from_chars=None):
         return PasswordGenerator.__generate_password(length)
 
 
 try:
-    p = PasswordGenerator.generate_password("7")
-    print(p)
+    p = PasswordGenerator.generate_password(5)
+    print(len(p), p)
 except Exception as e:
     print(e.args)
 
