3b336cdcaa6b51303ba1112a0e3a075e3d4ad65e
Hamidur Rahman <Hamidur0405@gmail.com>
2021-04-21 00:45:45 -0400

    fromstring var must instance of string

--- password_generator.py
+++ password_generator.py
@@ -15,27 +15,27 @@ class PasswordGenerator:
     def __generate_password(length=MIN_LENGTH, from_string=CHARS):
         """generates a random password of the given length. Length must be 7 or greater"""
 
-        try:
-            if length >= MIN_LENGTH:
-                return "".join(choice(CHARS) for x in range(randint(length, length)))
-            else:
-                return "".join(choice(CHARS) for x in range(randint(MIN_LENGTH, MIN_LENGTH)))
-        except ValueError:
-            raise ValueError("Expected integer but given {} for `length`.".format(type(length)))
+        if length >= MIN_LENGTH:
+            return "".join(choice(from_string) for x in range(randint(length, length)))
+        else:
+            return "".join(choice(from_string) for x in range(randint(MIN_LENGTH, MIN_LENGTH)))
 
     @staticmethod
-    def generate_password(length=MIN_LENGTH, from_string=CHARS):
+    def generate_password(length=MIN_LENGTH, source=CHARS, unique_source=False):
+
         try:
             length = int(length)
-            if isinstance(from_string, str):
-                return PasswordGenerator.__generate_password(length)
-            return PasswordGenerator.__generate_password(length)
+            if isinstance(source, str):
+                if unique_source:
+                    source = set(source)
+                    return PasswordGenerator.__generate_password(length, source)
+            return PasswordGenerator.__generate_password(length, source)
         except ValueError:
             raise ValueError("Expected integer but given {} for `length`".format(type(length)))
 
 
 try:
-    p = PasswordGenerator.generate_password(5, "as")
-    print(len(p), p)
+    p = PasswordGenerator.generate_password(7, "asDwK109?@")
+    print(p)
 except Exception as e:
     print(e.args)
